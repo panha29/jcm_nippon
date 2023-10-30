c8094f7f4b3fd62f1a533e7644def54b862a0faa
panha29 <78394799+panha29@users.noreply.github.com>
2023-10-30 17:46:35 +0700

    update

--- resources/views/website/services.blade.php
+++ resources/views/website/services.blade.php
@@ -14,45 +14,73 @@
         </div>
     </div>
     <div class="row mb-5">
-        <div class="col">
+        <div class="col" >
             <div class="card" style="border-radius: 0px !important">
                 <div class="row">
                     <div class="col">
                         <img src="img/service/service1.jpg" alt="nippon services">
                     </div>
                     <div class="col">
-                        <div class="card-body">
-                                <span>Color Scheme Service</span>
+                        <div class="container"style="text-align: left; padding:10px">
+                            <h5><b>Color Scheme Service</b></h5>
+                            <h5>Creating visually stunning and harmonious designs with inspiration.</h5>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="container">
+        <div class="col" >
+            <div class="card" style="border-radius: 0px !important">
+                <div class="row">
+                    <div class="col">
+                        <img src="img/service/service2.jpg" alt="nippon services">
+                    </div>
+                    <div class="col">
+                        <div class="container"style="text-align: left; padding:10px">
+                            <h5><b>Thousand Colors to Choose</b></h5>
+                            <h5>Choosing the right color for your house painting is a decision that should not be taken lightly.</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col" >
+            <div class="card" style="border-radius: 0px !important">
+                <div class="row">
+                    <div class="col">
+                        <img src="img/service/service3.jpg" alt="nippon services">
+                    </div>
+                    <div class="col">
+                        <div class="container"style="text-align: left; padding:10px">
+                            <h5><b>Computerized Color Mixing</b></h5>
+                            <h5>Keep forgetting your old color code? With our latest technology we can help.</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
+        <div class="col" >
             <div class="card" style="border-radius: 0px !important">
-                <img src="img/service/service3.jpg" alt="nippon services">
-                <div class="card-body span-text-responsive">
-                    <span>Computerized Color Mixing</span>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card" style="border-radius: 0px !important">
-                <img src="img/service/service4.jpg" alt="nippon services">
-                <div class="card-body span-text-responsive">
-                    <span>Painting Service</span>
+                <div class="row">
+                    <div class="col">
+                        <img src="img/service/service4.jpg" alt="nippon services">
+                    </div>
+                    <div class="col">
+                        <div class="container"style="text-align: left; padding:10px">
+                            <h5><b>Painting Service</b></h5>
+                            <h5>We also provide the painting services with </h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </div>
 
 <div class="container smaller-screen">
