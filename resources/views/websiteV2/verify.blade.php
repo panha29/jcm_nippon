d4fe38f7ca6f4ff19e87eb5e233436e3940afd82
panha29 <78394799+panha29@users.noreply.github.com>
2024-02-01 12:19:45 +0700

    update

--- resources/views/websiteV2/verify.blade.php
+++ resources/views/websiteV2/verify.blade.php
@@ -1,112 +1,112 @@
 <!doctype html>
-                        <html>
-                            <head>
-                                <meta charset='utf-8'>
-                                <meta name='viewport' content='width=device-width, initial-scale=1'>
-                                <title>Verify Page</title>
-                                <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
-                                <link href='' rel='stylesheet'>
-                                <style> @media screen {
-     @font-face {
-         font-family: 'Lato';
-         font-style: normal;
-         font-weight: 400;
-         src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
-     }
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>Verify Page</title>
+        <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='' rel='stylesheet'>
+        <style> @media screen {
+        @font-face {
+            font-family: 'Lato';
+            font-style: normal;
+            font-weight: 400;
+            src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
+        }
 
-     @font-face {
-         font-family: 'Lato';
-         font-style: normal;
-         font-weight: 700;
-         src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
-     }
+        @font-face {
+            font-family: 'Lato';
+            font-style: normal;
+            font-weight: 700;
+            src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
+        }
 
-     @font-face {
-         font-family: 'Lato';
-         font-style: italic;
-         font-weight: 400;
-         src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
-     }
+        @font-face {
+            font-family: 'Lato';
+            font-style: italic;
+            font-weight: 400;
+            src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
+        }
 
-     @font-face {
-         font-family: 'Lato';
-         font-style: italic;
-         font-weight: 700;
-         src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
-     }
- }
+        @font-face {
+            font-family: 'Lato';
+            font-style: italic;
+            font-weight: 700;
+            src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
+        }
+    }
 
- /* CLIENT-SPECIFIC STYLES */
- body,
- table,
- td,
- a {
-     -webkit-text-size-adjust: 100%;
-     -ms-text-size-adjust: 100%;
- }
+    /* CLIENT-SPECIFIC STYLES */
+    body,
+    table,
+    td,
+    a {
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+    }
 
- table,
- td {
-     mso-table-lspace: 0pt;
-     mso-table-rspace: 0pt;
- }
+    table,
+    td {
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+    }
 
- img {
-     -ms-interpolation-mode: bicubic;
- }
+    img {
+        -ms-interpolation-mode: bicubic;
+    }
 
- /* RESET STYLES */
- img {
-     border: 0;
-     height: auto;
-     line-height: 100%;
-     outline: none;
-     text-decoration: none;
- }
+    /* RESET STYLES */
+    img {
+        border: 0;
+        height: auto;
+        line-height: 100%;
+        outline: none;
+        text-decoration: none;
+    }
 
- table {
-     border-collapse: collapse !important;
- }
+    table {
+        border-collapse: collapse !important;
+    }
 
- body {
-     height: 100% !important;
-     margin: 0 !important;
-     padding: 0 !important;
-     width: 100% !important;
-     background-color: #f4f4f4;
- }
+    body {
+        height: 100% !important;
+        margin: 0 !important;
+        padding: 0 !important;
+        width: 100% !important;
+        background-color: #f4f4f4;
+    }
 
- /* iOS BLUE LINKS */
- a[x-apple-data-detectors] {
-     color: inherit !important;
-     text-decoration: none !important;
-     font-size: inherit !important;
-     font-family: inherit !important;
-     font-weight: inherit !important;
-     line-height: inherit !important;
- }
+    /* iOS BLUE LINKS */
+    a[x-apple-data-detectors] {
+        color: inherit !important;
+        text-decoration: none !important;
+        font-size: inherit !important;
+        font-family: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+    }
 
- /* MOBILE STYLES */
- @media screen and (max-width:600px) {
-     h1 {
-         font-size: 32px !important;
-         line-height: 32px !important;
-     }
- }
+    /* MOBILE STYLES */
+    @media screen and (max-width:600px) {
+        h1 {
+            font-size: 32px !important;
+            line-height: 32px !important;
+        }
+    }
 
- /* ANDROID CENTER FIX */
- div[style*="margin: 16px 0;"] {
-     margin: 0 !important;
- }
- .hiddenlink{
+    /* ANDROID CENTER FIX */
+    div[style*="margin: 16px 0;"] {
+        margin: 0 !important;
+    }
+    .hiddenlink{
 
-    display: none;
- }
- </style>
-                                <script type='text/javascript' src=''></script>
-                                <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-                                <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
-                            </head>
+        display: none;
+    }
+    </style>
+    <script type='text/javascript' src=''></script>
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+</head>
                             <body oncontextmenu='return false' class='snippet-body'>
                             <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
