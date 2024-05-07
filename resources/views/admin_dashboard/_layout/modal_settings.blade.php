f255ef12e81ab5e2abf3220ed6eb1cb1f24632c2
panha29 <78394799+panha29@users.noreply.github.com>
2024-05-07 17:08:57 +0700

    update

--- resources/views/admin_dashboard/_layout/modal_settings.blade.php
+++ resources/views/admin_dashboard/_layout/modal_settings.blade.php
@@ -552,16 +552,16 @@
 <!-- Niches Modal End -->
 
 <!-- Theme Settings & Niches Buttons Start -->
-<div class="settings-buttons-container">
+{{-- <div class="settings-buttons-container">
     <button type="button" class="btn settings-button btn-primary p-0" data-bs-toggle="modal" data-bs-target="#settings" id="settingsButton">
     <span class="d-inline-block no-delay" data-bs-delay="0" data-bs-offset="0,3" data-bs-toggle="tooltip" data-bs-placement="left" title="Settings">
       <i data-acorn-icon="paint-roller" class="position-relative"></i>
     </span>
     </button>
-    {{-- <button type="button" class="btn settings-button btn-primary p-0" data-bs-toggle="modal" data-bs-target="#niches" id="nichesButton">
+    <button type="button" class="btn settings-button btn-primary p-0" data-bs-toggle="modal" data-bs-target="#niches" id="nichesButton">
     <span class="d-inline-block no-delay" data-bs-delay="0" data-bs-offset="0,3" data-bs-toggle="tooltip" data-bs-placement="left" title="Niches">
       <i data-acorn-icon="toy" class="position-relative"></i>
     </span>
-    </button> --}}
-</div>
+    </button>
+</div> --}}
 <!-- Theme Settings & Niches Buttons End -->
