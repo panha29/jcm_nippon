9b3e7313d914dcb6a4147fa4ba4a9707523631d1
panha29 <78394799+panha29@users.noreply.github.com>
2025-01-09 17:13:53 +0700

    update

--- resources/views/websiteV2/my_maps.blade.php
+++ resources/views/websiteV2/my_maps.blade.php
@@ -7,6 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-
+    <iframe src="https://www.google.com/maps/d/embed?mid=1RIm_I4m7-K6zMS-1_TUlevLp4OUZ0mk&ehbc=2E312F" width="640" height="480"></iframe>
+    <embed src="https://www.youtube.com/" style="width:500px; height: 300px;">
+        <iframe src="https://www.youtube.com/" width="100%" height="300">
+            <p>Your browser does not support iframes.</p>
+          </iframe>
 </body>
 </html>
